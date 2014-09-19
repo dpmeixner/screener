919342fb8a691002ec56a69df52becef3113a6d9
David Meixner <dpmeixner@gmail.com>
2014-09-18 21:53:27 -0400

    Quick bug fix to models.py

--- screener/models.py
+++ screener/models.py
@@ -5,7 +5,7 @@ import shutil
 from pysec.models import Index
 from django.db import models
 
-#class Index(models.Model):
+class Index(models.Model):
 
     ticker = models.CharField(max_length=6,blank=True)
     cik = models.IntegerField()
